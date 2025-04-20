6b4cb5087202fc4686e247489da33a5182afecc7
Faqih Baidowi <faqihloh@gmail.com>
2025-04-20 13:00:03 +0700

    fix: add missing serviceWorkerRegistration.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Optional: You can include global styles here
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 // Create a root element to render the App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
